4e3f90e60ae77700072edc5a450a144cf89e8091
ArgentumPride <argentumpride@ukr.net>
2019-04-19 20:43:47 +0300

    Little fixes

--- src/main/java/ua/pride/models/Extra.java
+++ src/main/java/ua/pride/models/Extra.java
@@ -1,18 +1,5 @@
 package ua.pride.models;
 
 public enum Extra {
-    cleaning {
-        private int cost = 50;
-
-        public int getCost() {
-            return cost;
-        }
-
-        public void setCost(int cost) {
-            this.cost = cost;
-        }
-    },
-    breakfast {
-        public int cost = 70;
-    }
+    cleaning, breakfast
 }
